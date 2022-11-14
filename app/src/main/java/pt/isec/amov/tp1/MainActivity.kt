c7aa6447790fc7f0db4e19e05210a958cf5f4ab6
jRocha494 <jmarocha494@gmail.com>
2022-11-14 22:30:36 +0000

    Main Activity

--- app/src/main/java/pt/isec/amov/tp1/MainActivity.kt
+++ app/src/main/java/pt/isec/amov/tp1/MainActivity.kt
@@ -1,11 +1,35 @@
 package pt.isec.amov.tp1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import pt.isec.amov.tp1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnSingleplayer.setOnClickListener {
+            val intent = Intent(this, SingleplayerActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.btnMultiplayer.setOnClickListener {
+            val intent = Intent(this, MultiplayerActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.btnSettings.setOnClickListener {
+            val intent = Intent(this, SettingsActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.btnHistory.setOnClickListener {
+            val intent = Intent(this, HistoryActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/pt/isec/amov/tp1/SettingsActivity.kt
@@ -0,0 +1,11 @@
+package pt.isec.amov.tp1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+    }
+}
