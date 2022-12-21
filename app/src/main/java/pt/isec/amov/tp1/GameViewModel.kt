9df3f797129045db6801e62cc300b5e4c4bb957e
João Rocha <jmarocha494@gmail.com>
2022-12-21 18:51:21 +0000

    Functional Game with Level difficult increase

--- app/src/main/java/pt/isec/amov/tp1/GameViewModel.kt
+++ app/src/main/java/pt/isec/amov/tp1/GameViewModel.kt
@@ -1,5 +1,6 @@
 package pt.isec.amov.tp1
 
+import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import java.util.*
@@ -8,12 +9,16 @@ import kotlin.collections.HashMap
 
 class GameViewModel() : ViewModel() {
     var tab: ArrayList<String>
-    val results  = ArrayList<Double>()
-    lateinit var biggestResults: HashMap<Int, Double>
+    lateinit var results: ArrayList<Double>
+    lateinit var biggestResults: ArrayList<Double>
 
     var score = MutableLiveData<Int>()
     var levelNumber = MutableLiveData<Int>()
     var attempts = MutableLiveData<Int>()
+
+    private var minValue: Int
+    private var maxValue: Int
+    private var maxOperator: Int
 
     companion object {
         private const val BOARD_SIZE = 25
@@ -23,24 +28,31 @@ class GameViewModel() : ViewModel() {
         private const val DIVISION = "/"
         private const val WHITESPACE = ""
         private val BLANK_BOARD_POSITIONS = arrayOf(6, 8, 16, 18)
+        private const val TAG = "TAG"
     }
 
     init {
-        tab = populateGameTab(0, 9)
         score.value = 0
         levelNumber.value = 1
+        attempts.value = 0
+
+        minValue = 1
+        maxValue = 9
+        maxOperator = 0 //Primeiro nivel comeca por ter so adicao
+        tab = populateGameTab(this)
     }
 
-    fun populateGameTab(min: Int, max: Int): ArrayList<String>{
-        tab = ArrayList()
+    private fun populateGameTab(gameViewModel: GameViewModel): ArrayList<String>{
+        gameViewModel.tab = ArrayList()
+        gameViewModel.results = ArrayList()
 
         for(i in 0 until BOARD_SIZE){
             if(BLANK_BOARD_POSITIONS.contains(i))
-                tab.add(i, WHITESPACE)
+                gameViewModel.tab.add(i, WHITESPACE)
             else if(i % 2 == 0)
-                tab.add(i, randomNumberGenerator(min, max))
+                gameViewModel.tab.add(i, gameViewModel.randomNumberGenerator(minValue, maxValue))
             else
-                tab.add(i, randomOperatorGenerator())
+                gameViewModel.tab.add(i, gameViewModel.randomOperatorGenerator(maxOperator))
         }
 
         val expression = ArrayList<String>()
@@ -48,32 +60,34 @@ class GameViewModel() : ViewModel() {
         //Linhas
         for (i in 0 until BOARD_SIZE step 10){
             for (j in 0 until 5){
-                expression.add(tab[i+j])
+                expression.add(gameViewModel.tab[i+j])
             }
-            results.add(computeExpression(expression))
+            gameViewModel.results.add(gameViewModel.computeExpression(expression))
             expression.clear()
         }
 
         //Colunas
         for (j in 0 until 5 step 2){
             for (i in 0 until BOARD_SIZE step 5){
-                expression.add(tab[i+j])
+                expression.add(gameViewModel.tab[i+j])
             }
-            results.add(computeExpression(expression))
+            gameViewModel.results.add(gameViewModel.computeExpression(expression))
             expression.clear()
         }
 
-        var aux = results
-        aux.sort()
-        biggestResults = HashMap()
-        biggestResults.put(results.indexOf(aux[0]), aux[0])
-        biggestResults.put(results.indexOf(aux[1]), aux[1])
+        var aux = ArrayList<Double>()
+        aux.addAll(gameViewModel.results)
+        aux.sortDescending()
+        gameViewModel.biggestResults = ArrayList()
+        gameViewModel.biggestResults.add(aux[0])
+        gameViewModel.biggestResults.add(aux[1])
+        Log.d(TAG, "BIGGEST RESULTS: ${gameViewModel.results.indexOf(aux[0])},${gameViewModel.results.indexOf(aux[1])}") //LOG PARA DEBUG
 
-        return tab
+        return gameViewModel.tab
     }
 
     private fun computeExpression(expression : ArrayList<String>) : Double{
-        var result : Double = 0.0
+        var result = 0.0
 
         //verifica a ordem das operaçoes
         if (expression[1] == ADDITION || expression[1] == SUBTRACTION){
@@ -108,8 +122,8 @@ class GameViewModel() : ViewModel() {
         return (min..max).random().toString()
     }
 
-    private fun randomOperatorGenerator(): String {
-        val rand = randomNumberGenerator(0, 3)
+    private fun randomOperatorGenerator(max: Int): String {
+        val rand = randomNumberGenerator(0, maxOperator)
 
         return when (rand) {
             "0" -> ADDITION
@@ -127,36 +141,49 @@ class GameViewModel() : ViewModel() {
         var selectedResult = 0.0
 
         if (isHorizontal){
-            val row = (position / 10) % 10 //para verificar se está na primeira fila (posicoes 0 a 4), segunda (10 a 14) ou terceira (20 a 24)
-
-            selectedResult = results[row]
+            when(position) {
+                0, 1, 2, 3, 4 -> selectedResult = results[0] //resultado da primeira fila no array results
+                10, 11, 12, 13, 14 -> selectedResult = results[1] //resultado da segunda fila no array results
+                20, 21, 22, 23, 24 -> selectedResult = results[2] //resultado da terceira fila no array results
+                5, 6, 7, 8, 9, 15, 16, 17, 18, 19 -> return false
+            }
         }
         else{
-            val column = position % 10 //para verificar em que coluna está (0 - primeira coluna, 2 - segunda coluna, 4 - terceira coluna)
-
-            if(column % 2 != 0) // a coluna não pode ser 1 ou 3, pois estas nao tem expressões
-                return false
-
-            when(column) {
-                0 -> selectedResult = results[4] //resultado da primeira coluna no array results
-                2 -> selectedResult = results[5] //resultado da segunda coluna no array results
-                4 -> selectedResult = results[6] //resultado da terceira coluna no array results
+            when(position) {
+                0, 5, 10, 15, 20 -> selectedResult = results[3] //resultado da terc coluna no array results
+                2, 7, 12, 17, 22 -> selectedResult = results[4] //resultado da segunda coluna no array results
+                4, 9, 14, 19, 24 -> selectedResult = results[5] //resultado da terceira coluna no array results
+                1, 6, 11, 16, 21, 3, 8, 13, 18, 23 -> return false
             }
         }
 
-        if(selectedResult == biggestResults.get(0)) { //Melhor resultado
+        nextLevel(selectedResult)
+        return true
+    }
+
+    private fun nextLevel(selectedResult: Double){
+        if(selectedResult == biggestResults[0]) { //Melhor resultado
             score.value = (score.value)?.plus(2)
-        }else if (selectedResult == biggestResults.get(1)){ //Segundo melhor resultado
+        }else if (selectedResult == biggestResults[1]){ //Segundo melhor resultado
             score.value = (score.value)?.plus(1)
         }
 
         attempts.value = (attempts.value)?.plus(1)
         if(attempts.value == 5) { //Novo nivel ou perde caso sejam atingidas 5 tentativas
             levelNumber.value = (levelNumber.value)?.plus(1)
+
+            if(levelNumber.value!! < 5){
+                maxOperator += 1 //Passa a poder incluir mais um operador
+            }else{
+                if (levelNumber.value!! % 2 != 0)
+                    maxValue *= 11 //se o nivel for impar
+                else
+                    minValue *= 10
+            }
+
             attempts.value = 0
         }
 
-        tab = populateGameTab(0 , 9)
-        return true
+        tab = populateGameTab(this)
     }
 }
